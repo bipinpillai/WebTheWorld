abd2da0b7aa95ff36acc43e67622f8fe998dec2d
Bipin Pillai <bipin.pillai@gmail.com>
2015-12-25 06:52:12 -0600

    First Commit

---
+++ src/WebTheWorld/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebTheWorld
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync($"Hello World:   {context.Request.Path}");
+            //});
+
+            //            app.Run(async (context) =>
+            //            {
+            //                var html = @"<!DOCTYPE html>
+            //<html>
+            //<head></head>
+            //<body>
+            //    <h2> Webbing the world</h2>
+            //    <p>This should be interesting</p>
+            //</body>
+            //</html>";
+            //                await context.Response.WriteAsync(html);
+            //            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
---
+++ src/WebTheWorld/wwwroot/js/site.js
@@ -0,0 +1,82 @@
+﻿/*site.js*/
+
+function changeColor(id, newColor) {
+    $("#" + id).css("color", newColor);
+}
+
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+
+    var ele = $("#username");
+    ele.text(h + ":" + m + ":" + s);
+    //t = setTimeout(function () {
+    //    startTime()
+    //}, 500);
+}
+//startTime();
+
+(function() {
+    //startTime();
+
+    //var ele = document.getElementById("username");
+    //ele.innerHTML = "Siya1";
+   
+    //var main = document.getElementById("main");
+    //main.onmouseenter = function() {
+    //    main.style["background-color"] = "#888";
+    //};
+
+    //main.onmouseleave = function () {
+    //    main.style["background-color"] = "";
+    //};
+
+    var ele = $("#username");
+    ele.text("Siya22");
+    //ele.css("color", "#000");
+    //changeColor("username", "blue");
+
+    var main = $("#main");
+    //main.style = "background-color: #888;";
+    main.on("mouseenter", function() {
+        //main.css("background-color", "rgba(208, 230, 245, 1)");
+        main.css("background-color", "#888");
+    });
+
+    main.on("mouseleave", function () {
+        main.css("background-color", "");
+        //main.style = "";
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var t1 = $(this);
+        alert(t1.text());
+    });
+
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hideSidebar");
+        if ($sidebarAndWrapper.hasClass("hideSidebar")) {
+            $(this).text("Show sidebar");
+        } else {
+            $(this).text("Hide sidebar");
+        }
+
+    });
+
+})();
+
